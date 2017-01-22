eef3dfe61afd64d6ec3478bc66fb553b3f40cdd4
howdiz <howie@o3world.com>
2017-01-22 18:19:53 +0000

    initial commit
    working contact us inserts into db

---
+++ thanks.php
@@ -0,0 +1,62 @@
+<?php
+    
+    //Connect to the database
+    $host = "127.0.0.1";
+    $user = "howieross";                     //Your Cloud 9 username
+    $pass = "";                                  //Remember, there is NO password by default!
+    $db = "c9";                                  //Your database name you want to connect to
+    $port = 3306;                                //The port #. It is always 3306
+    
+    // Create connection
+    $conn = new mysqli($host, $user, $password, $db);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO contacts (name, email, message)
+    VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully <br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title> </title>
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="form.css" type="text/css" />
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                
+    </head>
+    <body>
+        <div class="container">
+            Thanks &nbsp; <?php echo $_POST["name"]; ?><br>
+            Your email address is: <?php echo $_POST["email"]; ?><br>
+            Your message is: <?php echo $_POST["message"]; ?>
+        </div>
+    </body>
+</html>    
